7bbfe3087cd6e2f0da1d3a60295d31e6b1320bb7
Rupesh Jaiswal <rupesh.jaiswal@sts.local>
2017-07-25 16:51:54 +0530

    First Draft

---
+++ src/js/ServicesList.js
@@ -0,0 +1,20 @@
+export default const servicesList=[
+{key:'Sleeper, metro tickets and seasonal passes',value:'0'},
+{key:'Outsourcing (in industries such as gems and jewellery, textiles)',value:'5'},
+{key:'Railways (AC)',value:'5'},
+{key:'Restaurants with annual turnover less than Rs. 50 lakhs',value:'5'},
+{key:'Cab aggregators like Ola, Uber',value:'5'},
+{key:'Hotels with tariff Rs. 1,000 - 2,500',value:'12'},
+{key:'Non-AC restaurants without liquor license',value:'12'},
+{key:'Real estate (Work contracts)',value:'12'},
+{key:'State run lotteries',value:'12'},
+{key:'Airlines (Business class)',value:'18'},
+{key:'Telecom, financial service',value:'18'},
+{key:'Hotel room tariff Rs. 2,500 - 7,500',value:'18'},
+{key:'Hotel room tariff above Rs.7,500',value:'28'},
+{key:'AC restaurants with liquor license',value:'18'},
+{key:'Movie tickets below Rs. 100',value:'18'},
+{key:'Movie tickets above Rs. 100',value:'28'},
+{key:'5 star hotels',value:'28'},
+{key:'State authorized lotteries',value:'28'}
+];
